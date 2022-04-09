a51b4991deeb788e998c5b3019a87eca19ebb395
Andres Merida <andresmerida1@gmail.com>
2022-04-09 02:00:07 -0400

    webModule dev Add new api for test (#2)

---
+++ bus-web-module/src/main/java/com/ultrasoft/bus/core/BusCoreApplication.java
@@ -0,0 +1,64 @@
+package com.ultrasoft.bus.core;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Optional;
+
+@SpringBootApplication
+public class BusCoreApplication {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(BusCoreApplication.class);
+
+    public static void main(String[] args) {
+        var springApplication = new SpringApplication(BusCoreApplication.class);
+        Environment environment = springApplication.run(args).getEnvironment();
+        logApplicationStartUp(environment);
+    }
+
+    private static void logApplicationStartUp(Environment env) {
+        var protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store"))
+                .map(key -> "https")
+                .orElse("http");
+        var serverPort = env.getProperty("server.port");
+        var contextPath = Optional
+                .ofNullable(env.getProperty("server.servlet.context-path"))
+                .filter(StringUtils::isNotBlank)
+                .orElse("/");
+        var hostAddress = "localhost";
+
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOGGER.warn("The host name could not be determined, using 'localhost' as fallback");
+        }
+
+        var textBlock = """
+                
+                ----------------------------------------------------------
+                    Application "%s" is running! Access URLs:
+                    Local:              %s://localhost:%s%s
+                    External:           %s://%s:%s%s
+                    Active Profile(s):  %s
+                    Default Profile(s): %s
+                ----------------------------------------------------------
+                """.formatted(env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                Arrays.toString(env.getActiveProfiles()),
+                Arrays.toString(env.getDefaultProfiles()));
+        LOGGER.info(textBlock);
+    }
+}
